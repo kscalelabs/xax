bbfc0d8615bb12acf1ce6e5e1d4c0b70e87c8c6c
Benjamin Bolte <ben@bolte.cc>
2025-03-13 18:32:45 +0800

    more functions into xax

--- xax/__init__.py
+++ xax/__init__.py
@@ -79,6 +79,7 @@ __all__ = [
     "stage_environment",
     "to_markdown_table",
     "jit",
+    "quat_to_euler",
     "ColoredFormatter",
     "configure_logging",
     "one_hot",
@@ -194,6 +195,7 @@ NAME_MAP: dict[str, str] = {
     "stage_environment": "utils.experiments",
     "to_markdown_table": "utils.experiments",
     "jit": "utils.jax",
+    "quat_to_euler": "utils.jax",
     "ColoredFormatter": "utils.logging",
     "configure_logging": "utils.logging",
     "one_hot": "utils.numpy",
@@ -313,7 +315,7 @@ if IMPORT_ALL or TYPE_CHECKING:
         stage_environment,
         to_markdown_table,
     )
-    from xax.utils.jax import jit
+    from xax.utils.jax import jit, quat_to_euler
     from xax.utils.logging import (
         LOG_ERROR_SUMMARY,
         LOG_PING,
