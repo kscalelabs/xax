c52d9bf224bfa6c278b45fdcd51804884cfb2067
Benjamin Bolte <ben@bolte.cc>
2025-03-14 14:31:46 +0800

    unit test

---
+++ tests/utils/test_jaxpr.py
@@ -0,0 +1,25 @@
+"""Tests for the JAXPR utilities."""
+
+from pathlib import Path
+
+import jax
+import jax.numpy as jnp
+
+import xax
+
+
+def test_save_jaxpr_dot(tmpdir: Path) -> None:
+    def loss_fn(x: jnp.ndarray) -> jnp.ndarray:
+        return jnp.sum(x**2)
+
+    jaxpr = jax.make_jaxpr(loss_fn)(jnp.array([1.0, 2.0, 3.0]))
+    filename = tmpdir / "test_jaxpr.dot"
+    xax.save_jaxpr_dot(jaxpr, filename)
+
+    with open(filename, "r") as f:
+        contents = f.read()
+
+    # Ensures that the .dot file is formatted correctly (this is not a very
+    # comprehensive test, we should probably improve it).)
+    assert contents.startswith("digraph Jaxpr {")
+    assert contents.endswith("}\n")
