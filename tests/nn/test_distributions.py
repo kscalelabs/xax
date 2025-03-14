088677d0d9955ce5ba3107eea51b514f3c9f6c8f
Benjamin Bolte <ben@bolte.cc>
2025-03-14 14:04:42 +0800

    eh

--- tests/nn/test_distributions.py
+++ tests/nn/test_distributions.py
@@ -8,4 +8,12 @@ import xax
 
 
 def test_gaussian_distribution() -> None:
-    raise NotImplementedError
+    """Test the Gaussian distribution."""
+    distribution = xax.GaussianDistribution(action_dim=2)
+    parameters = jnp.array([0.0, 1.0, 0.0, 1.0])
+    actions = jnp.array([0.0, 0.0])
+    assert jnp.allclose(
+        distribution.log_prob(parameters, actions),
+        -0.5 * jnp.square((actions - parameters[:2]) / parameters[2:]),
+        atol=1e-6,
+    )
