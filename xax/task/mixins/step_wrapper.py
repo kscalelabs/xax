763dbc99f459c3d621f28076485c2d3c20311767
Ben Bolte <ben@bolte.cc>
2025-03-20 05:35:22 -0700

    step context (#37)
    * step context
    
    * fixes
    
    * lint

--- xax/task/mixins/step_wrapper.py
+++ xax/task/mixins/step_wrapper.py
@@ -1,53 +1,39 @@
 """Defines a mixin to wrap some steps in a context manager."""
 
+import time
 from dataclasses import dataclass
 from types import TracebackType
-from typing import ContextManager, Literal, TypeVar
+from typing import Callable, ContextManager, TypeVar
 
-import equinox as eqx
 import jax
 
 from xax.task.base import BaseConfig, BaseTask
-
-StepType = Literal[
-    "backward",
-    "change_mode",
-    "clip_grads",
-    "create_optimizers",
-    "forward",
-    "get_dataloader",
-    "get_dataset",
-    "get_prefetcher",
-    "get_model",
-    "get_optimizer",
-    "get_initial_opt_state",
-    "get_update_fn",
-    "load_checkpoint",
-    "log_losses",
-    "model_to_device",
-    "on_step_end",
-    "on_step_start",
-    "save_checkpoint",
-    "step",
-    "update_state",
-    "write_logs",
-    "zero_grads",
-]
 
 
 class StepContext(ContextManager):
     """Context manager to get the current step type."""
 
-    CURRENT_STEP: StepType | None = None
+    CURRENT_STEP: str | None = None
 
-    def __init__(self, step: StepType) -> None:
+    def __init__(
+        self,
+        step: str,
+        on_context_start: Callable[[str], None],
+        on_context_end: Callable[[str, float], None],
+    ) -> None:
         self.step = step
+        self.start_time = 0.0
+        self.on_context_start = on_context_start
+        self.on_context_end = on_context_end
 
     def __enter__(self) -> None:
         StepContext.CURRENT_STEP = self.step
+        self.start_time = time.time()
+        self.on_context_start(self.step)
 
     def __exit__(self, _t: type[BaseException] | None, _e: BaseException | None, _tr: TracebackType | None) -> None:
         StepContext.CURRENT_STEP = None
+        self.on_context_end(self.step, time.time() - self.start_time)
 
 
 @jax.tree_util.register_dataclass
@@ -63,6 +49,11 @@ class StepContextMixin(BaseTask[Config]):
     def __init__(self, config: Config) -> None:
         super().__init__(config)
 
-    @eqx.filter_jit
-    def step_context(self, step: StepType) -> ContextManager:
-        return StepContext(step)
+    def step_context(self, step: str) -> ContextManager:
+        return StepContext(step, self.on_context_start, self.on_context_stop)
+
+    def on_context_start(self, step: str) -> None:
+        pass
+
+    def on_context_stop(self, step: str, elapsed_time: float) -> None:
+        pass
