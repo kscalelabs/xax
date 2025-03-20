3982f55a3140c185966c073977e159d5daf01b43
Benjamin Bolte <ben@bolte.cc>
2025-03-19 21:14:32 -0700

    small fix in histogram logic

--- xax/task/logger.py
+++ xax/task/logger.py
@@ -687,8 +687,8 @@ class Logger:
             values = value() if callable(value) else value
 
             if isinstance(values, Array):
-                values = np.array(values)  # Convert JAX array to numpy array
-                counts, limits = np.histogram(values, bins=bins)
+                counts, limits = jnp.histogram(values, bins=bins)
+                counts, limits = as_numpy(counts), as_numpy(limits)
             elif isinstance(values, np.ndarray):
                 counts, limits = np.histogram(values, bins=bins)
             else:
