de61deeb01695d31d8789d20eee1cd7062658daf
Ben Bolte <ben@bolte.cc>
2025-04-02 22:07:34 -0700

    make load_checkpoint into a classmethod (#58)
    * make load_checkpoint into a classmethod
    
    * fix up overloads

--- xax/task/base.py
+++ xax/task/base.py
@@ -82,9 +82,6 @@ class BaseTask(Generic[Config]):
     def on_after_checkpoint_save(self, ckpt_path: Path, state: State) -> State:
         return state
 
-    def on_before_checkpoint_load(self, ckpt_path: Path) -> None:
-        pass
-
     @functools.cached_property
     def task_class_name(self) -> str:
         return self.__class__.__name__
