6c24b6102090e35c1bba244223570c5eca081c82
Benjamin Bolte <ben@bolte.cc>
2025-04-12 21:13:45 -0700

    avoid int32 overflow

--- xax/core/state.py
+++ xax/core/state.py
@@ -14,6 +14,10 @@ from xax.core.conf import field
 Phase = Literal["train", "valid"]
 
 
+def _clip_int(i: int) -> int:
+    return max(min(i, 2**31 - 1), -(2**31))
+
+
 def _phase_to_int(phase: Phase) -> int:
     return {"train": 0, "valid": 1}[phase]
 
@@ -36,42 +40,42 @@ class StateDict(TypedDict, total=False):
 @jax.tree_util.register_dataclass
 @dataclass(frozen=True, kw_only=True)
 class State:
-    _int64_arr: Array = field(MISSING, help="Internal array for storing int64 values")
-    _float64_arr: Array = field(MISSING, help="Internal array for storing floating-point values")
+    _int32_arr: Array = field(MISSING, help="Internal array for storing int64 values")
+    _float32_arr: Array = field(MISSING, help="Internal array for storing floating-point values")
 
     @property
     def num_steps(self) -> int:
-        return self._int64_arr[0].item()
+        return self._int32_arr[0].item()
 
     @property
     def num_samples(self) -> int:
-        return self._int64_arr[1].item()
+        return self._int32_arr[1].item()
 
     @property
     def num_valid_steps(self) -> int:
-        return self._int64_arr[2].item()
+        return self._int32_arr[2].item()
 
     @property
     def num_valid_samples(self) -> int:
-        return self._int64_arr[3].item()
+        return self._int32_arr[3].item()
 
     @property
     def start_time_s(self) -> float:
-        return self._float64_arr[0].item()
+        return self._float32_arr[0].item()
 
     @property
     def elapsed_time_s(self) -> float:
-        return self._float64_arr[1].item()
+        return self._float32_arr[1].item()
 
     @property
     def phase(self) -> Phase:
-        return _int_to_phase(self._int64_arr[6])
+        return _int_to_phase(self._int32_arr[6].item())
 
     @classmethod
     def init_state(cls) -> "State":
         return cls(
-            _int64_arr=jnp.array([0, 0, 0, 0, 0, 0, 0], dtype=jnp.int64),
-            _float64_arr=jnp.array([time.time(), 0.0], dtype=jnp.float64),
+            _int32_arr=jnp.array([0, 0, 0, 0, 0, 0, 0], dtype=jnp.int32),
+            _float32_arr=jnp.array([time.time(), 0.0], dtype=jnp.float32),
         )
 
     @property
@@ -88,30 +92,31 @@ class State:
                 raise ValueError(f"Invalid phase: {phase}")
 
     def replace(self, **kwargs: Unpack[StateDict]) -> "State":
-        int64_arr = self._int64_arr
-        float64_arr = self._float64_arr
+        int32_arr = self._int32_arr
+        float32_arr = self._float32_arr
 
         if "num_steps" in kwargs:
-            int64_arr.at[0].set(kwargs["num_steps"])
+            int32_arr.at[0].set(_clip_int(kwargs["num_steps"]))
         if "num_samples" in kwargs:
-            int64_arr.at[1].set(kwargs["num_samples"])
+            int32_arr.at[1].set(_clip_int(kwargs["num_samples"]))
         if "num_valid_steps" in kwargs:
-            int64_arr.at[2].set(kwargs["num_valid_steps"])
+            int32_arr.at[2].set(_clip_int(kwargs["num_valid_steps"]))
         if "num_valid_samples" in kwargs:
-            int64_arr.at[3].set(kwargs["num_valid_samples"])
+            int32_arr.at[3].set(_clip_int(kwargs["num_valid_samples"]))
+
         if "phase" in kwargs:
-            int64_arr.at[6].set(_phase_to_int(kwargs["phase"]))
+            int32_arr.at[6].set(_phase_to_int(kwargs["phase"]))
         if "_phase" in kwargs:
-            int64_arr.at[6].set(kwargs["_phase"])
+            int32_arr.at[6].set(kwargs["_phase"])
 
         if "start_time_s" in kwargs:
-            float64_arr.at[0].set(kwargs["start_time_s"])
+            float32_arr.at[0].set(kwargs["start_time_s"])
         if "elapsed_time_s" in kwargs:
-            float64_arr.at[1].set(kwargs["elapsed_time_s"])
+            float32_arr.at[1].set(kwargs["elapsed_time_s"])
 
         return State(
-            _int64_arr=int64_arr,
-            _float64_arr=float64_arr,
+            _int32_arr=int32_arr,
+            _float32_arr=float32_arr,
         )
 
     def to_dict(self) -> dict[str, int | float | str]:
@@ -130,26 +135,26 @@ class State:
         if "phase" in d:
             d["_phase"] = _phase_to_int(cast(Phase, d.pop("phase")))
 
-        int64_arr = jnp.array(
+        int32_arr = jnp.array(
             [
-                d.get("num_steps", 0),
-                d.get("num_samples", 0),
-                d.get("num_valid_steps", 0),
-                d.get("num_valid_samples", 0),
-                d.get("_phase", 0),
+                _clip_int(d.get("num_steps", 0)),
+                _clip_int(d.get("num_samples", 0)),
+                _clip_int(d.get("num_valid_steps", 0)),
+                _clip_int(d.get("num_valid_samples", 0)),
+                _clip_int(d.get("_phase", 0)),
             ],
-            dtype=jnp.int64,
+            dtype=jnp.int32,
         )
 
-        float64_arr = jnp.array(
+        float32_arr = jnp.array(
             [
                 d.get("start_time_s", time.time()),
                 d.get("elapsed_time_s", 0.0),
             ],
-            dtype=jnp.float64,
+            dtype=jnp.float32,
         )
 
         return cls(
-            __int64_arr=int64_arr,
-            __float64_arr=float64_arr,
+            _int32_arr=int32_arr,
+            _float32_arr=float32_arr,
         )
