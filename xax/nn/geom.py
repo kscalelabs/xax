fd0fb03c26661c0ffc094c3561327de3f99a6892
budzianowski <budzianowski@gmail.com>
2025-04-18 14:44:36 -0700

    add 6d continuous representation

--- xax/nn/geom.py
+++ xax/nn/geom.py
@@ -1,5 +1,6 @@
 """Defines geometry functions."""
 
+import chex
 from jax import numpy as jnp
 from jaxtyping import Array
 
@@ -211,3 +212,49 @@ def quat_to_rotmat(quat: Array, eps: float = 1e-6) -> Array:
         ],
         axis=-2,
     )
+
+
+def normalize(v: jnp.ndarray, axis: int = -1, eps: float = 1e-8) -> jnp.ndarray:
+    norm = jnp.linalg.norm(v, axis=axis, keepdims=True)
+    return v / jnp.clip(norm, a_min=eps)
+
+
+def rotation6d_to_rotation_matrix(r6d: jnp.ndarray) -> jnp.ndarray:
+    """Convert 6D rotation representation to rotation matrix.
+
+    From https://arxiv.org/pdf/1812.07035, Appendix B
+
+    Args:
+        r6d: The 6D rotation representation, shape (*, 6).
+
+    Returns:
+        The rotation matrix, shape (*, 3, 3).
+    """
+    chex.assert_shape(r6d, (..., 6))
+    shape = r6d.shape
+    flat = r6d.reshape(-1, 6)
+    x_raw = flat[:, 0:3]
+    y_raw = flat[:, 3:6]
+    x = normalize(x_raw, axis=-1)
+    z = jnp.cross(x, y_raw)
+    z = normalize(z, axis=-1)
+    y = jnp.cross(z, x)
+
+    mat = jnp.stack([x, y, z], axis=-1)  # [N,3,3]
+    return mat.reshape(shape[:-1] + (3, 3))
+
+
+def rotation_matrix_to_rotation6d(rotation_matrix: jnp.ndarray) -> jnp.ndarray:
+    """Convert rotation matrix to 6D rotation representation.
+
+    Args:
+        rotation_matrix: The rotation matrix, shape (*, 3, 3).
+
+    Returns:
+        The 6D rotation representation, shape (*, 6).
+    """
+    chex.assert_shape(rotation_matrix, (..., 3, 3))
+    shape = rotation_matrix.shape
+    r6d = jnp.concatenate([rotation_matrix[..., 0], rotation_matrix[..., 1]], axis=-1)
+
+    return r6d.reshape(shape[:-2] + (6,))
