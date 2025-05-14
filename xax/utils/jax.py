04c028011a8e3bbe959a6d38edd8cf8b24dfb75d
Benjamin Bolte <ben@bolte.cc>
2025-05-14 16:28:41 -0400

    correct split_args

--- xax/utils/jax.py
+++ xax/utils/jax.py
@@ -270,8 +270,8 @@ def vmap(
         if not all(isinstance(a, int) or a is None for a in ia):
             raise ValueError("in_axes must be a list of integers or None")
 
-        split_args = [a if i is None else _split_module(a, axis=i) for i, a in zip(ia, args, strict=True)]
-        split_outputs = [fun(*sargs, **kwargs) for sargs in zip(*split_args, strict=False)]
+        split_args = [[a] * len(args) if i is None else _split_module(a, axis=i) for i, a in zip(ia, args, strict=True)]
+        split_outputs = [fun(*sargs, **kwargs) for sargs in zip(*split_args, strict=True)]
 
         if not split_outputs:
             return jnp.array([])  # type: ignore[return-value]
